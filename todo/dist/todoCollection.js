10eaf4c518620770d7aac9e7a4f546b2f626af41
Imagion <algernoncowperwood@gmail.com>
2024-05-04 08:50:49 +0300

    removing completed items

--- todo/dist/todoCollection.js
+++ todo/dist/todoCollection.js
@@ -4,22 +4,24 @@ exports.TodoCollection = void 0;
 const todoItem_1 = require("./todoItem");
 class TodoCollection {
     userName;
-    todoItems;
     nextId = 1;
+    itemMap = new Map();
     constructor(userName, todoItems = []) {
         this.userName = userName;
-        this.todoItems = todoItems;
-        // no statements required
+        todoItems.forEach((item) => this.itemMap.set(item.id, item));
     }
     addTodo(task) {
         while (this.getTodoById(this.nextId)) {
             this.nextId++;
         }
-        this.todoItems.push(new todoItem_1.TodoItem(this.nextId, task));
+        this.itemMap.set(this.nextId, new todoItem_1.TodoItem(this.nextId, task));
         return this.nextId;
     }
     getTodoById(id) {
-        return this.todoItems.find((item) => item.id === id);
+        return this.itemMap.get(id);
+    }
+    getTodoItems(includeComplete) {
+        return [...this.itemMap.values()].filter((item) => includeComplete || !item.complete);
     }
     markComplete(id, complete) {
         const todoItem = this.getTodoById(id);
