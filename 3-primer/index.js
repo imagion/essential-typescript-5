bcbfc7d00fcff60967521f5f015779ee8ac52472
Imagion <algernoncowperwood@gmail.com>
2024-05-06 12:08:13 +0300

    nullish operator

--- 3-primer/index.js
+++ 3-primer/index.js
@@ -4,9 +4,9 @@ let bootsPrice = '100';
 console.log(`Boots price: ${bootsPrice}`);
 
 let taxRate; // no tax rate has been defined
-console.log(`Tax rate: ${taxRate || 10}%`);
+console.log(`Tax rate: ${taxRate ?? 10}%`);
 taxRate = 0; // zero-rated for tax
-console.log(`Tax rate: ${taxRate || 10}%`);
+console.log(`Tax rate: ${taxRate ?? 10}%`);
 
 if (hatPrice === bootsPrice) {
   console.log('Prices are the same');
