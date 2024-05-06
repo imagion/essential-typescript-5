ca8dcaaa2f7ee6a7c459590ea3574fe0908c29e6
Imagion <algernoncowperwood@gmail.com>
2024-05-06 12:07:10 +0300

    falsy coersion

--- 3-primer/index.js
+++ 3-primer/index.js
@@ -3,6 +3,11 @@ console.log(`Hat price: ${hatPrice}`);
 let bootsPrice = '100';
 console.log(`Boots price: ${bootsPrice}`);
 
+let taxRate; // no tax rate has been defined
+console.log(`Tax rate: ${taxRate || 10}%`);
+taxRate = 0; // zero-rated for tax
+console.log(`Tax rate: ${taxRate || 10}%`);
+
 if (hatPrice === bootsPrice) {
   console.log('Prices are the same');
 } else {
@@ -16,3 +21,7 @@ let myVariable = 'Adam';
 console.log(`Type: ${typeof myVariable}`);
 myVariable = 100;
 console.log(`Type: ${typeof myVariable}`);
+
+let firstCity;
+let secondCity = firstCity || 'London';
+console.log(`City: ${secondCity}`);
