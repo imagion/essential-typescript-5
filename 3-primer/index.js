fea83b7c6bcfae6a8b4e3e5e5bf26db0ce204770
Imagion <algernoncowperwood@gmail.com>
2024-05-06 15:02:24 +0300

    destructuring

--- 3-primer/index.js
+++ 3-primer/index.js
@@ -1,21 +1,8 @@
 let names = ['Hat', 'Boots', 'Gloves'];
-let prices = [];
 
-prices.push(100);
-prices.push('100');
-prices.push(50.25);
+let [, , three] = names;
+console.log(`Three: ${three}`);
 
-console.log(`First Item: ${names[0]}: ${prices[0]}`);
-
-let sumPrices = (...numbers) =>
-  numbers.reduce(
-    (total, val) => total + (Number.isNaN(Number(val)) ? 0 : Number(val)),
-  );
-
-let totalPrice = sumPrices(...prices);
-console.log(`Total: ${totalPrice} ${typeof totalPrice}`);
-
-let combinedArray = [...names, ...prices];
-combinedArray.forEach((element) =>
-  console.log(`Combined Array Element: ${element}`),
-);
+let prices = [100, 120, 50.25];
+let [, ...highest] = prices.sort((a, b) => a - b);
+highest.forEach((price) => console.log(`High price: ${price}`));
