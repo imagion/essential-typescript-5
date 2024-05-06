68b42cd635c868b8192c43149020533d54ed21db
Imagion <algernoncowperwood@gmail.com>
2024-05-06 20:00:14 +0300

    this methods

--- 3-primer/index.js
+++ 3-primer/index.js
@@ -1,8 +1,13 @@
-function writeMessage(message) {
-  console.log(`${this.greeting}, ${message}`);
-}
+let myObject = {
+  greeting: 'Hi, there',
+
+  writeMessage(message) {
+    console.log(`${this.greeting}, ${message}`);
+  },
+};
 
 greeting = 'Hello';
+myObject.writeMessage('It is sunny today');
 
-writeMessage('It is sunny today');
-writeMessage.call(global, 'It is sunny today');
+let myFunction = myObject.writeMessage;
+myFunction('It is sunny today');
