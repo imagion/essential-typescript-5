397d48f5a7be0213b0d3a6cd56b9786bf13dc9db
Imagion <algernoncowperwood@gmail.com>
2024-05-06 11:26:28 +0300

    preventing coercing

--- 3-primer/index.js
+++ 3-primer/index.js
@@ -3,11 +3,16 @@ console.log(`Hat price: ${hatPrice}`);
 let bootsPrice = '100';
 console.log(`Boots price: ${bootsPrice}`);
 
-if (hatPrice == bootsPrice) {
+if (hatPrice === bootsPrice) {
   console.log('Prices are the same');
 } else {
   console.log('Prices are different');
 }
 
-let totalPrice = hatPrice + bootsPrice;
+let totalPrice = Number(hatPrice) + Number(bootsPrice);
 console.log(`Total Price: ${totalPrice}`);
+
+let myVariable = 'Adam';
+console.log(`Type: ${typeof myVariable}`);
+myVariable = 100;
+console.log(`Type: ${typeof myVariable}`);
