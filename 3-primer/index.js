fb990090f17639473344ec0634af23cd0525ab6a
Imagion <algernoncowperwood@gmail.com>
2024-05-06 14:57:47 +0300

    arrays

--- 3-primer/index.js
+++ 3-primer/index.js
@@ -1,29 +1,21 @@
-let hatPrice = 100;
-console.log(`Hat price: ${hatPrice}`);
-let bootsPrice = '100';
-console.log(`Boots price: ${bootsPrice}`);
+let names = ['Hat', 'Boots', 'Gloves'];
+let prices = [];
 
-let taxRate; // no tax rate has been defined
-console.log(`Tax rate: ${taxRate ?? 10}%`);
-taxRate = 0; // zero-rated for tax
-console.log(`Tax rate: ${taxRate ?? 10}%`);
+prices.push(100);
+prices.push('100');
+prices.push(50.25);
 
-if (hatPrice === bootsPrice) {
-  console.log('Prices are the same');
-} else {
-  console.log('Prices are different');
-}
+console.log(`First Item: ${names[0]}: ${prices[0]}`);
 
 let sumPrices = (...numbers) =>
   numbers.reduce(
     (total, val) => total + (Number.isNaN(Number(val)) ? 0 : Number(val)),
   );
 
-let totalPrice = sumPrices(hatPrice, bootsPrice);
+let totalPrice = sumPrices(...prices);
 console.log(`Total: ${totalPrice} ${typeof totalPrice}`);
 
-totalPrice = sumPrices(100, 200, 300);
-console.log(`Total: ${totalPrice} ${typeof totalPrice}`);
-
-totalPrice = sumPrices(100, 200);
-console.log(`Total: ${totalPrice} ${typeof totalPrice}`);
+let combinedArray = [...names, ...prices];
+combinedArray.forEach((element) =>
+  console.log(`Combined Array Element: ${element}`),
+);
