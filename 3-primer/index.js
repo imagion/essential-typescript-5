d59278cab6568e72022fa48bbdb03ed305284964
Imagion <algernoncowperwood@gmail.com>
2024-05-06 12:13:48 +0300

    function

--- 3-primer/index.js
+++ 3-primer/index.js
@@ -19,4 +19,8 @@ function sumPrices(first, second, third) {
 }
 
 let totalPrice = sumPrices(hatPrice, bootsPrice);
-console.log(`Total Price: ${totalPrice}`);
+console.log(`Total: ${totalPrice} ${typeof totalPrice}`);
+totalPrice = sumPrices(100, 200, 300);
+console.log(`Total: ${totalPrice} ${typeof totalPrice}`);
+totalPrice = sumPrices(100, 200);
+console.log(`Total: ${totalPrice} ${typeof totalPrice}`);
