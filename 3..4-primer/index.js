cf9b87270ffd1060e54eb1a2cad94180c30fb2f2
Imagion <algernoncowperwood@gmail.com>
2024-05-07 14:54:02 +0300

    object prototype

--- 3..4-primer/index.js
+++ 3..4-primer/index.js
@@ -5,4 +5,22 @@ let hat = {
     return Number(this.price) * 1.2;
   },
 };
+
+let boots = {
+  name: 'Boots',
+  price: 100,
+  getPriceIncTax() {
+    return Number(this.price) * 1.2;
+  },
+};
+
+let hatPrototype = Object.getPrototypeOf(hat);
+console.log(`Hat Prototype: ${hatPrototype}`);
+
+let bootsPrototype = Object.getPrototypeOf(boots);
+console.log(`Boots Prototype: ${bootsPrototype}`);
+
+console.log(`Common prototype: ${hatPrototype === bootsPrototype}`);
+
 console.log(`Hat: ${hat.price}, ${hat.getPriceIncTax()}`);
+console.log(`toString: ${hat.toString()}`);
