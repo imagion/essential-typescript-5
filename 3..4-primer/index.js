2440e73e208546696b9f06e0fde6d30157735945
Imagion <algernoncowperwood@gmail.com>
2024-05-10 14:19:40 +0300

    static method

--- 3..4-primer/index.js
+++ 3..4-primer/index.js
@@ -23,10 +23,13 @@ class TaxedProduct extends Product {
     let chainResult = super.toString();
     return `${chainResult}, Tax: ${this.getPriceIncTax()}`;
   }
+
+  static process(...products) {
+    products.forEach((p) => console.log(p.toString()));
+  }
 }
 
-let hat = new TaxedProduct('Hat', 100);
-let boots = new TaxedProduct('Boots', 100, 1.3);
-
-console.log(hat.toString());
-console.log(boots.toString());
+TaxedProduct.process(
+  new TaxedProduct('Hat', 100, 1.2),
+  new TaxedProduct('Boots', 100),
+);
