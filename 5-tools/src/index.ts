98c1be3510de216199a2c1b75890ee63c0100452
Imagion <algernoncowperwood@gmail.com>
2024-05-12 21:45:26 +0300

    es5 compile

--- 5-tools/src/index.ts
+++ 5-tools/src/index.ts
@@ -1,4 +1,9 @@
-function printMessage(msg: string): void {
-  console.log(`Message: ${msg}`);
-}
-printMessage('Hello, TypeScript');
+import { sum } from './calc';
+
+let printMessage = (msg: string): void => console.log(`Message: ${msg}`);
+
+let message = 'Hello, TypeScript';
+printMessage(message);
+
+let total = sum(100, 200, 300);
+console.log(`Total: ${total}`);
