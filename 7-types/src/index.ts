12fc29f8410b6bd4373475b511cb684d083e524f
Imagion <algernoncowperwood@gmail.com>
2024-05-15 14:10:25 +0300

    using file declaration

--- 7-types/src/index.ts
+++ 7-types/src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello, TypeScript');
+function calculateTax(amount: number) {
+  return (amount * 1.2).toFixed(2);
+}
+
+let price = 100;
+let taxAmount = calculateTax(price);
+let halfShare = taxAmount / 2;
+
+console.log(`Price: ${price}`);
+console.log(`Full amount in tax: ${taxAmount}`);
+console.log(`Half share: ${halfShare}`);
