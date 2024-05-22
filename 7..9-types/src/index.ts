5762ccfa216d53b350bbd1e341b9c6180a102440
Imagion <algernoncowperwood@gmail.com>
2024-05-22 19:53:55 +0300

    template literal string types

--- 7..9-types/src/index.ts
+++ 7..9-types/src/index.ts
@@ -9,23 +9,11 @@ function getRandomValue(): 1 | 2 | 3 | 4 {
   return (Math.floor(Math.random() * 4) + 1) as 1 | 2 | 3 | 4;
 }
 
-enum City {
-  London = 'LON',
-  Paris = 'PAR',
-  Chicago = 'CHI',
+function getCityString(
+  city: 'London' | 'Paris' | 'Chicago',
+): `City: ${'London' | 'Paris' | 'Chicago'}` {
+  return `City: ${city}`;
 }
 
-function getMixedValue(): 1 | 'Hello' | true | City.London {
-  switch (getRandomValue()) {
-    case 1:
-      return 1;
-    case 2:
-      return 'Hello';
-    case 3:
-      return true;
-    case 4:
-      return City.London;
-  }
-}
-
-console.log(`Value: ${getMixedValue()}`);
+let str = getCityString('London');
+console.log(str);
