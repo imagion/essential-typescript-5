f9c5e633589d4a4621072373dd50e5f9c2652379
Imagion <algernoncowperwood@gmail.com>
2024-05-23 11:47:51 +0300

    methods in shape type

--- 7..10-types/src/index.ts
+++ 7..10-types/src/index.ts
@@ -1,15 +1,25 @@
+enum Feature {
+  Waterproof,
+  Insulated,
+}
+
 let hat = { name: 'Hat', price: 100 };
 let gloves = { name: 'Gloves', price: 75 };
-let umbrella = { name: 'Umbrella', price: 30, waterproof: true };
+let umbrella = {
+  name: 'Umbrella',
+  price: 30,
+  hasFeature: (feature) => feature === Feature.Waterproof,
+};
 
-let products: { name: string; price?: number; waterproof?: boolean }[] = [
-  hat,
-  gloves,
-  umbrella,
-];
+let products: {
+  name: string;
+  price?: number;
+  hasFeature?(Feature): boolean;
+}[] = [hat, gloves, umbrella];
 
 products.forEach((prod) =>
   console.log(
-    `${prod.name}: ${prod.price} ` + `Waterproof: ${prod.waterproof}`,
+    `${prod.name}: ${prod.price} ` +
+      `${prod.hasFeature ? prod.hasFeature(Feature.Waterproof) : 'false'}`,
   ),
 );
