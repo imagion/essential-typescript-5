032eca60da5dcc26f95dbf4b4784c65318a59d9a
Imagion <algernoncowperwood@gmail.com>
2024-05-23 08:50:23 +0300

    object shape type

--- 7..10-types/src/index.ts
+++ 7..10-types/src/index.ts
@@ -1,6 +1,7 @@
 let hat = { name: 'Hat', price: 100 };
 let gloves = { name: 'Gloves', price: 75 };
+let umbrella = { name: 'Umbrella' };
 
-let products = [hat, gloves];
+let products: { name: string; price: number }[] = [hat, gloves, umbrella];
 
 products.forEach((prod) => console.log(`${prod.name}: ${prod.price}`));
