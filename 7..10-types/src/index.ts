79eb8e6b04ebd4ee01c4ef17bd2b84c057639e70
imagion <algernoncowperwood@gmail.com>
2024-07-04 12:57:36 +0300

    type guard for objects

--- 7..10-types/src/index.ts
+++ 7..10-types/src/index.ts
@@ -10,16 +10,19 @@ type Person = {
   city: string;
 };
 
-type UnionType = {
-  id: number | string;
-  name: string;
-};
-
 let hat = { id: 1, name: 'Hat', price: 100 };
 let gloves = { id: 2, name: 'Gloves', price: 75 };
 let umbrella = { id: 3, name: 'Umbrella', price: 30 };
 let bob = { id: 'bsmith', name: 'Bob', city: 'London' };
 
-let dataItems: UnionType[] = [hat, gloves, umbrella, bob];
+let dataItems: (Product | Person)[] = [hat, gloves, umbrella, bob];
+
+dataItems.forEach((item) => {
+  if ('city' in item) {
+    console.log(`Person: ${item.name}: ${item.city}`);
+  } else {
+    console.log(`Product: ${item.name}: ${item.price}`);
+  }
+});
 
 dataItems.forEach((item) => console.log(`ID: ${item.id}, Name: ${item.name}`));
