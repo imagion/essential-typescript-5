51a55932ce45e2de119d25928ccfb60de6e3447f
imagion <algernoncowperwood@gmail.com>
2024-07-05 14:59:13 +0300

    started chapter 11

--- 7..10-types/src/index.ts
+++ 7..10-types/src/index.ts
@@ -2,32 +2,14 @@ type Person = {
   id: string;
   name: string;
   city: string;
-  getContact(field: string): string;
 };
 
-type Employee = {
-  id: string;
-  company: string;
-  dept: string;
-  getContact(field: number): number;
-};
+let data: Person[] = [
+  { id: 'bsmith', name: 'Bob Smith', city: 'London' },
+  { id: 'ajones', name: 'Alice Jones', city: 'Paris' },
+  { id: 'dpeters', name: 'Dora Peters', city: 'New York' },
+];
 
-type EmployedPerson = Person & Employee;
-
-let person: EmployedPerson = {
-  id: 'bsmith',
-  name: 'Bob Smith',
-  city: 'London',
-  company: 'Acme Co',
-  dept: 'Sales',
-  getContact(field: string | number): any {
-    return typeof field === 'string' ? 'Alice' : 6512346543;
-  },
-};
-
-let typeTest = person.getContact;
-let stringParamTypeTest = person.getContact('Alice');
-let numberParamTypeTest = person.getContact(123);
-
-console.log(`Contact: ${person.getContact('Alice')}`);
-console.log(`Contact: ${person.getContact(12)}`);
+data.forEach((item) => {
+  console.log(`${item.id} ${item.name}, ${item.city}`);
+});
