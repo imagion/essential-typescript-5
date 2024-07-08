f4f3d8fffd6641ed40f01e0ee305c77df95d1b8a
imagion <algernoncowperwood@gmail.com>
2024-07-08 11:02:46 +0300

    get set accessors

--- 7..11-types/src/index.ts
+++ 7..11-types/src/index.ts
@@ -5,25 +5,33 @@ type Person = {
 };
 
 class Employee {
-  public readonly id: string;
-  public name: string;
-  private dept: string;
   private city: string;
 
-  constructor(id: string, name: string, dept: string, city: string) {
-    this.id = id;
-    this.name = name;
-    this.dept = dept;
+  constructor(
+    public readonly id: string,
+    public name: string,
+    private dept: string,
+    city: string,
+  ) {
     this.city = city;
   }
 
   writeDept() {
     console.log(`${this.name} works in ${this.dept}`);
   }
+
+  get location() {
+    return this.city;
+  }
+
+  set location(newCity) {
+    this.city = newCity;
+  }
 }
 
 let salesEmployee = new Employee('fvega', 'Fidel Vega', 'Sales', 'Paris');
 
 salesEmployee.writeDept();
-
-salesEmployee.id = 'fidel';
+console.log(`Location: ${salesEmployee.location}`);
+salesEmployee.location = 'London';
+console.log(`Location: ${salesEmployee.location}`);
