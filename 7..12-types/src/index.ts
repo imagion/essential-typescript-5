c04f1bc8648af80c3380ba1f4a8bf385f85dbc02
imagion <algernoncowperwood@gmail.com>
2024-07-12 12:17:12 +0300

    using a generic type

--- 7..12-types/src/index.ts
+++ 7..12-types/src/index.ts
@@ -6,4 +6,22 @@ let people = [
 ];
 let products = [new Product('Running Shoes', 100), new Product('Hat', 25)];
 
-[...people, ...products].forEach((item) => console.log(`Item: ${item.name}`));
+class DataCollection<T> {
+  private items: T[] = [];
+  constructor(initialItems: T[]) {
+    this.items.push(...initialItems);
+  }
+
+  add(newItem: T) {
+    this.items.push(newItem);
+  }
+
+  getItem(index: number): T {
+    return this.items[index];
+  }
+}
+
+let peopleData = new DataCollection<Person>(people);
+
+let firstPerson = peopleData.getItem(0);
+console.log(`First Person: ${firstPerson.name}, ${firstPerson.city}`);
