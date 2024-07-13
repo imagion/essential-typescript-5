4dbc71d533abaf2f3db09ef57b3880677c395c55
imagion <algernoncowperwood@gmail.com>
2024-07-13 11:35:30 +0300

    iterating objects

--- 7..13-types/src/index.ts
+++ 7..13-types/src/index.ts
@@ -6,22 +6,35 @@ type shapeType = { name: string };
 
 class Collection<T extends shapeType> {
   private items: Map<string, T>;
+
   constructor(initialItems: T[] = []) {
     this.items = new Map<string, T>();
     this.add(...initialItems);
   }
+
   add(...newItems: T[]): void {
     newItems.forEach((newItem) => this.items.set(newItem.name, newItem));
   }
+
   get(name: string): T {
     return this.items.get(name);
   }
+
   get count(): number {
     return this.items.size;
+  }
+
+  values(): Iterator<T> {
+    return this.items.values();
   }
 }
 
 let productCollection: Collection<Product> = new Collection(products);
 console.log(`There are ${productCollection.count} products`);
-let p = productCollection.get('Hat');
-console.log(`Product: ${p.name}, ${p.price}`);
+
+let iterator: Iterator<Product> = productCollection.values();
+let result: IteratorResult<Product> = iterator.next();
+while (!result.done) {
+  console.log(`Product: ${result.value.name}, ${result.value.price}`);
+  result = iterator.next();
+}
