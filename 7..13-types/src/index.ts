8b2426919245bb1c0db168f8037539ce9d74c0c8
imagion <algernoncowperwood@gmail.com>
2024-07-14 11:49:11 +0300

    Combining transformations

--- 7..13-types/src/index.ts
+++ 7..13-types/src/index.ts
@@ -1,13 +1,15 @@
 import { City, Person, Product, Employee } from './dataTypes.js';
 
-type MakeReadWrite<T> = {
-  -readonly [P in keyof T]: T[P];
+type CustomMapped<T, K extends keyof T> = {
+  readonly [P in K]?: T[P];
 };
 
-type optionalType = Partial<Product>;
-type requiredType = Required<optionalType>;
-type readOnlyType = Readonly<requiredType>;
-type readWriteType = MakeReadWrite<readOnlyType>;
+type BuiltInMapped<T, K extends keyof T> = Readonly<Partial<Pick<T, K>>>;
 
-let p: readWriteType = { name: 'Kayak', price: 275 };
-console.log(`Mapped type: ${p.name}, ${p.price}`);
+let p1: CustomMapped<Product, 'name'> = { name: 'Kayak' };
+let p2: BuiltInMapped<Product, 'name' | 'price'> = {
+  name: 'Lifejacket',
+  price: 48.95,
+};
+console.log(`Custom mapped type: ${p1.name}`);
+console.log(`Built-in mapped type: ${p2.name}, ${p2.price}`);
