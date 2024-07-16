74c2b07b5c66df0eff6f5ad3c7b58940b1fddc29
imagion <algernoncowperwood@gmail.com>
2024-07-16 12:56:00 +0300

    chapter 14 started

---
+++ 14-decorators/src/city.ts
@@ -0,0 +1,6 @@
+export class City {
+  constructor(public name: string, public population: number) {}
+  getSummary(): string {
+    return `Name: ${this.name}, Population: ${this.population}`;
+  }
+}
---
+++ 14-decorators/src/product.ts
@@ -0,0 +1,7 @@
+export class Product {
+  constructor(public name: string, public price: number) {}
+
+  getDetails(): string {
+    return `Name: ${this.name}, Price: $${this.price}`;
+  }
+}
