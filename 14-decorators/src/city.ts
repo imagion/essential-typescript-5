52d063e38cfd3616f4c1072be386e1fac9a4b104
imagion <algernoncowperwood@gmail.com>
2024-07-17 17:24:16 +0300

    using decorators

--- 14-decorators/src/city.ts
+++ 14-decorators/src/city.ts
@@ -1,5 +1,8 @@
+import { time } from './methodDecorator.js';
 export class City {
   constructor(public name: string, public population: number) {}
+
+  @time
   getSummary(): string {
     return `Name: ${this.name}, Population: ${this.population}`;
   }
--- 14-decorators/src/product.ts
+++ 14-decorators/src/product.ts
@@ -1,7 +1,15 @@
+import { time } from './methodDecorator.js';
+
 export class Product {
   constructor(public name: string, public price: number) {}
 
+  @time
   getDetails(): string {
     return `Name: ${this.name}, Price: $${this.price}`;
+  }
+
+  @time
+  getPrice(): number {
+    return this.price;
   }
 }
