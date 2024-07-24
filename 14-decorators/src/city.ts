c5571d24d564f140d2617e41a47205a96ad20bb5
imagion <algernoncowperwood@gmail.com>
2024-07-24 13:24:37 +0300

    applying multiple decorators

--- 14-decorators/src/city.ts
+++ 14-decorators/src/city.ts
@@ -1,7 +1,10 @@
 import { time } from './methodDecorator.js';
+import { message } from './multiples.js';
 export class City {
   constructor(public name: string, public population: number) {}
 
+  @message('First Decorator')
+  @message('Second Decorator')
   @time()
   getSummary(): string {
     return `Name: ${this.name}, Population: ${this.population}`;
