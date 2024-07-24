a589245aca998ed5c5612c729cf9fef02555cd32
imagion <algernoncowperwood@gmail.com>
2024-07-24 13:33:32 +0300

    using an initializer

--- 14-decorators/src/index.ts
+++ 14-decorators/src/index.ts
@@ -2,16 +2,10 @@ import { City } from './city.js';
 import { Product } from './product.js';
 import { isSerializeable } from './classDecorator.js';
 
-let city = new City('London', 8_982_000);
+//let city = new City("London", 8_982_000);
 let product = new Product('Kayak', 275);
 
-console.log(city.getSummary());
-// console.log(product.getDetails());
-// console.log(`Price: ${product.getPrice()}`);
+//console.log(city.getSummary());
+console.log(product.getDetails());
 
-// console.log(`Get Product tax: ${product.tax}`);
-// product.tax = 30;
-
-// if (isSerializeable(product)) {
-//   product.serialize();
-// }
+console.log(`Price: ${product.getPrice()}`);
