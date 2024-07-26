ee9df955be9826c512bca5082a367ab784a16c7a
imagion <algernoncowperwood@gmail.com>
2024-07-26 15:02:47 +0300

    type declaration files

---
+++ 15-usingjs/src/formatters.d.ts
@@ -0,0 +1,5 @@
+export declare function sizeFormatter(thing: string, count: number): void;
+export declare function costFormatter(
+  thing: string,
+  cost: number | string,
+): void;
--- 15-usingjs/src/index.ts
+++ 15-usingjs/src/index.ts
@@ -12,5 +12,4 @@ cart.addProduct(hat, 1);
 cart.addProduct(hat, 2);
 
 sizeFormatter('Cart', cart.itemCount);
-// costFormatter('Cart', cart.totalPrice);
 costFormatter('Cart', `${cart.totalPrice}`);
