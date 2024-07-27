3241cd207e4bf571a8fca800888f8ccb43f33ec6
imagion <algernoncowperwood@gmail.com>
2024-07-27 13:21:43 +0300

    packages that include type declarations

--- 15-usingjs/src/index.ts
+++ 15-usingjs/src/index.ts
@@ -2,6 +2,7 @@ import { SportsProduct, SPORT } from './product.js';
 import { Cart } from './cart.js';
 import { sizeFormatter, costFormatter } from './formatters.js';
 import debug from 'debug';
+import chalk from 'chalk';
 
 let kayak = new SportsProduct(1, 'Kayak', 275, SPORT.Watersports);
 let hat = new SportsProduct(2, 'Hat', 22.1, SPORT.Running, SPORT.Watersports);
@@ -15,6 +16,5 @@ cart.addProduct(hat, 2);
 sizeFormatter('Cart', cart.itemCount);
 costFormatter('Cart', `${cart.totalPrice}`);
 
-let db = debug('Example App');
-db.enabled = true;
-db('Message: %s', 'Test message');
+console.log(chalk.greenBright('Formatted message'));
+// console.log(chalk.notAColor('Formatted message'));
