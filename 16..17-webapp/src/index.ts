212654e39dc7c9e68e44909551a7cc2521f30b74
imagion <algernoncowperwood@gmail.com>
2024-08-05 18:55:32 +0300

    adding a web service

--- 16..17-webapp/src/index.ts
+++ 16..17-webapp/src/index.ts
@@ -1,14 +1,12 @@
-import { LocalDataSource } from './data/localDataSource';
+import { RemoteDataSource } from './data/remoteDataSource';
 import { HtmlDisplay } from './htmlDisplay';
 import 'bootstrap/dist/css/bootstrap.css';
 
-let ds = new LocalDataSource();
+let ds = new RemoteDataSource();
 
 async function displayData(): Promise<HTMLElement> {
   let display = new HtmlDisplay();
   display.props = {
-    // products: await ds.getProducts('name'),
-    // order: ds.order,
     dataSource: ds,
   };
   return display.getContent();
